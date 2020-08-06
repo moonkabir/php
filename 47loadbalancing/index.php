b27d6d4303d8bd95d0c70c63d6246a83c0a50c10
MoonKabir <45933210+moonkabir@users.noreply.github.com>
2020-08-07 00:09:54 +0600

    Update index.php

--- 47loadbalancing/index.php
+++ 47loadbalancing/index.php
@@ -1,1 +1,1 @@
-load balancing diye server er request load manage kora r data manage kora
+load balancing diye server er request load manage kora r data manage kora and nginx diye data server e deploy kora
